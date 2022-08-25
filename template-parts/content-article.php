c73bce6df3d37248bd43f53d9263c9ab16510504
Amir Liaqat <90547462+AmirAliLiaqat@users.noreply.github.com>
2022-08-25 08:45:45 -0700

    Adding new updated files...

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,5 +1,4 @@
 <?php
-
     if(have_posts()) {
         while(have_posts()) {
             the_post();
@@ -26,22 +25,22 @@
     <div class="post-footer">
         <span class="facebook_share">
             <a href="https://www.facebook.com/sharer/sharer.php?u=<?php site_url(); ?>/<?php the_permalink(); ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/facebook_like.png"/>
+                <img src="<?php echo THEME_DIR_URI; ?>/images/facebook_like.png"/>
             </a>
         </span>
         <span class="twitter_share">
             <a href="https://twitter.com/intent/tweet?original_referer=<?php site_url(); ?>/&ref_src=twsrc%5Etfw&text=&tw_p=tweetbutton&url=<?php site_url(); ?>/<?php the_permalink(); ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/tweet.png"/>
+                <img src="<?php echo THEME_DIR_URI; ?>/images/tweet.png"/>
             </a>
         </span>
         <span class="linkedin_share hidden-xs">
             <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php site_url(); ?>/<?php the_permalink(); ?>/&title=&summary=1.%20Hit%20%E2%80%9CWindows%20Key%20+%20R%E2%80%9D%20to%20open%20the%20run%20dialogue%20box.%202.%20Enter%C2%A0diskpart%C2%A0and%20click%20%E2%80%9COK%E2%80%9D%20to%20open%20a%20black%20command%20prompt%20window.%20%E2%80%9Clist%20disk%E2%80%9D%20(It%20displays%20all%20the%20disks%20of%20your%20computer.%20)%20%E2%80%9Csel%20disk%200%E2%80%9D%20(...">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/linked_share.png"/>
+                <img src="<?php echo THEME_DIR_URI; ?>/images/linked_share.png"/>
             </a>
         </span>
         <span class="google_share hidden-xs">
             <a href="https://plus.google.com/share?url=<?php site_url(); ?>/<?php the_permalink(); ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/google-plus-icon.png"/>
+                <img src="<?php echo THEME_DIR_URI; ?>/images/google-plus-icon.png"/>
             </a>
         </span>
         <span class="post_comments">
